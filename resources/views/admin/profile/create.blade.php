e83d38faaec703bb04ccae366390be900b71b711
EC2 Default User <ec2-user@ip-172-31-19-196.us-east-2.compute.internal>
2020-11-02 12:50:45 +0000

    課題提出

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -5,10 +5,10 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-wide, initial-scale=1">
         
-        <title>MyNews</title>
+        <title>MyProfile</title>
     </head>
     <body>
-        <h1>Myニュース作成画面</h1>
+        <h1>Myprofile作成画面</h1>
     </body>
 </html>
 
@@ -16,15 +16,15 @@
 @extends('layouts.admin')
 
 
-{{-- admin.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
-@section('title', 'ニュースの新規作成')
+{{-- admin.blade.phpの@yield('title')に'profileの新規作成'を埋め込む --}}
+@section('title', 'profileの新規作成')
 
 {{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
 @section('content')
     <div class="container">
         <div class="row">
             <div class="col-md-8 mx-auto">
-                <h2>ニュース新規作成</h2>
+                <h2>profile新規作成</h2>
             </div>
         </div>
     </div>
